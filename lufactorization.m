3fc8fb4bda176ec1ff3c2c0b21ed7c638dca81e6
ChampManZ <champ_tnt@hotmail.com>
2023-02-21 09:37:03 +0700

    add basic

--- lufactorization.m
+++ lufactorization.m
@@ -1,2 +1,13 @@
 %% LU Factorization | 63011342 Thanapat Eurboonyanun
 
+% Theory Note: U comes from the elimination e.g. (E32E31E21)A = U
+% L comes from its inverse
+
+function x = lufactorization(A, b)
+    disp('Original matrix A: ');
+    disp(A);
+    [m, n] = size(A);
+    L = eye(n);  % Lower
+    P = L;  % Permutation
+    U = A;  % Upper
+end
