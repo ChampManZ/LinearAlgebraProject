7d9c29623b4e9a181278807b035ac215d9673860
bright-luminous <72394943+bright-luminous@users.noreply.github.com>
2023-04-24 12:01:44 +0700

    fix attem2

--- echelon.m
+++ echelon.m
@@ -1,24 +1,34 @@
 function [outputArr] = echelon(inputArr)
 %ECHELON Summary of this function goes here
 %   Detailed explanation goes here
-size = length(inputArr);
-for j=1:size-1
+size1 = length(inputArr);
+
+% Create an augmented matrix with identity matrix as the right-hand side
+n = size(inputArr,1);
+inputArr = horzcat(inputArr, eye(n));
+
+for j=1:size1-1
+    if inputArr(j, j) == 0
+        outputArr = [];
+        return;
+    end
 %disp(tempArr)
-    for i = j+1:size
+    for i = j+1:size1
         tempArr = inputArr(j,:)*(inputArr(i,j)/inputArr(j,j));
         inputArr(i,:)= inputArr(i,:)- tempArr;
         %disp(inputArr(i,j)/inputArr(j,j))
     end
 end
-for i=1:size
-    for j=1:size
+for i=1:size1
+    for j=1:size1
         if abs(inputArr(i,j))<=0.001
             inputArr(i,j) = 0;
         end
     end
 end
 
-disp(inputArr)
+%disp(inputArr)
+%outputArr = inputArr(:, n+1:end);
 outputArr = inputArr;
 end
 
--- test_script.m
+++ test_script.m
@@ -1,12 +1,24 @@
-x=randi([-50,50],15);
+x=randi([-5000,5000],5);
 %x = [1 2 3; 2 5 2; 6 -3 1;];
 disp(x)
 
-%y=matrix_invert_self(x);
+y=matrix_invert_self(x);
 %y=det_self(x);
 %z=det_self2(x);
-y=echelon(x);
+%y=echelon(x);
+anw = x^-1;
 
 disp(y)
 disp(x^-1)
+for i=1:length(x)
+    for j=1:length(x)
+        if y(i,j)~= anw(i,j)
+            disp("correct")
+        else
+            disp("nah")
+            disp(y(i,j))
+            disp(anw(i,j))
+        end
+    end
+end
 %disp(re(x))
